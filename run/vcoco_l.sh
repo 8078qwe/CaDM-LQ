c492a4417d1758316e87bcd1f5ab6d1110ec9e8b
8078qwe <xh1m22@std.uestc.edu.cn>
2026-01-30 13:45:07 +0800

    Add files via upload

---
+++ run/vcoco_l.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -x
+export SD_Config="/path/to/v1-inference.yaml"
+export SD_ckpt="/path/to/v1-5-pruned-cali.ckpt"
+EXP_DIR=exps/cadm_l_vcoco
+
+python -m torch.distributed.launch \
+        --nproc_per_node=8 \
+        --use_env \
+        main.py \
+        --pretrained params/detr-r101-pre-2branch-vcoco.pth \
+        --model_name cadm_L \
+        -c configs/DiffHOI_L.py \
+        --output_dir ${EXP_DIR} \
+        --dataset_file vcoco \
+        --hoi_path data/v-coco \
+        --num_obj_classes 81 \
+        --num_verb_classes 29 \
+        --backbone swin_L_384_22k \
+        --num_queries 900 \
+        --dec_layers 3 \
+        --epochs 90 \
+        --lr_drop 60 \
+        --use_nms_filter \
+        --ft_clip_with_small_lr \
+        --with_clip_label \
+        --with_obj_clip_label \
+        --use_cadiff_kd \
+        --lambda_soft 350 \
+        --kd_eps 1e-6 \
+        --kd_gamma 2.0
